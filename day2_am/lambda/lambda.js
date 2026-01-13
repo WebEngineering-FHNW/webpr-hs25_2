659a5d0f14d70998756d7cbd8a3e93e043df76bc
Dierk Koenig <dierk.koenig@canoo.com>
2026-01-13 12:26:08 +0100

    after day 2 am

--- day2_am/lambda/lambda.js
+++ day2_am/lambda/lambda.js
@@ -1,5 +1,25 @@
+const id    = x => x;
+const konst = x => y => x;
+const snd   = x => y => y;
 
+const T = konst;
+const F = snd;
 
+const and = a => b => a ( b ) ( a );
+const or  = a => b => a ( a ) ( b );
+
+const flip = f => x => y => f(y)(x);
+const not  = a => flip (a) (T) (F);
+
+const beq = a => b => a (b) (not(b));
+
+const Pair = x => y => f => f(x)(y) ;
+const firstname = konst;
+const lastname  = snd;
+
+const Left   =  x => lh => rh => lh(x);
+const Right  =  x => lh => rh => rh(x);
+const either =  id;
 
 // ----- special -----
 
